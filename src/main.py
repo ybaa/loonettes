66efef7990550d9589afd9b94c33a659ec801c8b
Martyna Łagożna <m.lagozna@gmail.com>
2019-04-29 23:13:11 +0200

    run learning session on multi class classifier with 4 channels

--- src/main.py
+++ src/main.py
@@ -9,9 +9,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # turn off warnings
 # exp1.test_detection()
 
 classes = ['backpack', 'bike', 'book', 'chair', 'coach', 'cup', 'phone', 'skateboard']
-mlc = []
 for cls in classes:
-    mlc.append(CNNMyDatasetMultiLabel(cls))
-
-for single_classifier in mlc:
-    single_classifier.run_learning_session(save=True)
+    mlc = CNNMyDatasetMultiLabel(cls)
+    mlc.run_learning_session(save=True)
